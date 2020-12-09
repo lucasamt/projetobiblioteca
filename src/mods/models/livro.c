727af718b595d73118c37f566874481d3fb3405e
Renato Junior <levandeoficial@gmail.com>
2020-12-08 21:28:37 -0300

    Models

---
+++ src/mods/models/livro.c
@@ -0,0 +1,7 @@
+typedef struct
+{
+  char nome_Livro[T];
+  char nome_Autor[T];
+  char data_Publicacao[T];
+  int emprestado_por;
+} Livro;
---
+++ src/mods/models/usuario.c
@@ -0,0 +1,6 @@
+typedef struct
+{
+  char nome_Aluno[T];
+  char ra[T];
+  int id;
+} Usuario;
